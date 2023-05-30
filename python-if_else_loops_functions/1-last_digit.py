ac31f4adbb3844381d1ded1645604a2eb9b51508
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-30 10:15:09 +0100

    fixing spaces

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ else:
 if (x > 5):
     print(f"Last digit of {number} is {x} and is greater than 5")
 elif (x == 0):
-    print(f"Last digit of {number} is {x} and is  0")
+    print(f"Last digit of {number} is {x} and is 0")
 elif (x < 6):
     print(f"Last digit of {number} is {x} and is less than 6 and not 0")
