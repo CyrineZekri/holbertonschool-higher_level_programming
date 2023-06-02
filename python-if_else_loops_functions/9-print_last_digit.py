58fbab137eac1b02e914c2d95e67be68514b131c
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-02 11:03:21 +0100

    last

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-
-    if number > 0:
-        x = number % 10
-        print(x, end="")
-        return (x)
-    elif number < 0:
-        x = (number * -1) % 10
-        print(x, end="")
-        return (x)
-    else:
-        x = 0
-        return (x)
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
