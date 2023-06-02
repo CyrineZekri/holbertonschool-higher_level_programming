741ebebf10de115adef40081e6ecc5235c89fdc5
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-02 14:00:15 +0100

    upeer

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    upper = ""
-    for i in range(len(str)):
-        if (ord(str[i]) in range(97, 123)):
-            upper += chr(ord(str[i]-32))
-        else:
-            upper += str[i]
-    print("{}".format(str))
+        f = ""
+        for i in range(len(str)):
+            if (ord(str[i]) in range(97, 123)):
+                f += chr(ord(str[i]) - 32)
+            else:
+                f += str[i]
+        print("{}".format(f))
