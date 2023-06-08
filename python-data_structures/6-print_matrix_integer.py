1797b49e260cc35a0b0a24093dbb5cc4ba038480
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-08 10:18:58 +0100

    matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
     for innerlist in matrix:
         for element in innerlist:
             print(element, end=",")
