310feef6dfb57994e1cb49a73ab159415128b963
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-09 10:25:39 +0100

    space

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        print("")
     for i in range(len(matrix)):
+        if len(matrix[i]) == 0:
+            print("")
         for j in range(len(matrix[i])):
             if j == len(matrix[i])-1:
                 print("{:d}".format(matrix[i][j]))
