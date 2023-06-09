30361e93e189c54614839bcc090d7903de1f4a1a
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-09 23:44:23 +0100

    excepte

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    c = 0
+def safe_print_integer(value):
     try:
-        for i in range(x):
-            print("{}".format(my_list[i]), end="")
-            c = c + 1
+        print("{:d}".format(value))
+        B = True
     except:
-        pass
-    finally:
-        print()
-    return(c)
+        B = False
+    return(B)
