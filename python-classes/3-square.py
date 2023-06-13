9dd3c1acecf427e55fff9007d0e63e2285cf044a
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-13 23:03:03 +0100

    4th

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,7 +13,7 @@ class Square:
         Attributes:
             __size (int): The size of the square.
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
