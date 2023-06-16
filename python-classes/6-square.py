f2dff12b6527646d3473feb291d6c2bf541023fd
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-16 17:53:52 +0100

    representation

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,13 +31,8 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(1, self.__size ** 2 + 1):
-            if i == 1:
-                for j in range(self.__position[0]):
-                    print(" ", end="")
-            if i % self.__size == 0:
-                print("#")
-                for j in range(self.__position[0]):
-                    print(" ", end="")
-            else:
-                print("#", end="")
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
