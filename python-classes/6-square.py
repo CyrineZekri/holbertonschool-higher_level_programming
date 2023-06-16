4c8b7e20b26b2aa8e41d25cd4b40ab9a69d72131
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-16 18:48:04 +0100

    last

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -32,7 +32,9 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            if self.__position[1] > 0:
+                for i in range(self.__size):
+                    print('#' * self.__size)
+            else:
+                for i in range(self.__size):
+                    print(" " * self.__position[0] + "#"*self.__size)
