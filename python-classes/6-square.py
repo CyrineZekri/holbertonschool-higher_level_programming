39b8c43052b7eff49d1802f07a3cbc1a9378e164
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-16 17:09:00 +0100

    print

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,42 +1,32 @@
 #!/usr/bin/python3
-""" Class Defining a square"""
+""" Class Definition"""
 
 
 class Square:
-    "private attribute : size"
+    """Defines a class square"""
+    __size = None
+    __position = None
 
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
+        if type(position) != tuple or (position[0] < 0 or position[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
     @property
     def size(self):
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property
-    def position(self):
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-
     def area(self):
-
-        return self.__size**2
+        return (self.__size ** 2)
 
     def my_print(self):
         if self.__size == 0:
