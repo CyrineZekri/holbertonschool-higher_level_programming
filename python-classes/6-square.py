6eec837ea6eb56aa744ec29a4db150a2c2e61d3e
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-16 19:10:51 +0100

    last

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -14,6 +14,19 @@ class Square:
         self.__position = position
 
     @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        x, y = value
+        if not isinstance(x, int) or not isinstance(y, int) or x < 0 or y < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    @property
     def size(self):
         return (self.__size)
 
