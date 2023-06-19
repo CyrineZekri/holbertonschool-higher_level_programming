2b378add1db3c2ca424e9e41669ef6ffc19a0615
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-19 11:25:49 +0100

    fixed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Module add_integer: Contains a function to add two int or float numbers.
+"""
+
+
 def add_integer(a, b=98):
-    """Function that adds two int or float numbers"""
+    """
+    Function that adds two int or float numbers
+       Raises a TypeError if a or b is not an integer or float.
+       Returns the sum of a and b.
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
