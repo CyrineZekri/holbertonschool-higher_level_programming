1ff1e11170f2667967a14a43388aab74651bf574
CyrineZekri <cyrinezekri4@gmail.com>
2023-06-20 11:03:01 +0100

    code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 """
+
 module that contains the function : text_indentation
+
 """
 
 
 def text_indentation(text):
     """
-    indents two lines after :  . ? and :
+    indents text after special characters
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    text = text.strip()
     characters = ['.', '?', ':']
     returned = ""
-    for char in text:
+    for i, char in enumerate(text):
         returned += char
-        if char in characters:
+        if char in characters and i < len(text)-1:
             returned += "\n\n"
-    print(returned.strip())
+    print(returned)
