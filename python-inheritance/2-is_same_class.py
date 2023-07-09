0dda8851d193684814e7d0b4241ff9b9b1dff2f4
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-09 11:54:44 +0100

    pycodes style corrected

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+module that checks if object is exactly
+an instance of a specified class
+
+"""
+
+
 def is_same_class(obj, a_class):
-    if type(obj) is a_class:
+    """
+    checks
+    """
+    if isinstance(obj, a_class):
         return True
     else:
         return False
