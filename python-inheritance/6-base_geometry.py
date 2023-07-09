1ec379dcb04cc6d78f71373de06b60d963501d4b
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-09 15:10:10 +0100

    task 6 completed

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module with geometry basics
+
+
+"""
+
+
+class BaseGeometry:
+    """
+    class that contains geometry
+    """
+
+    def area(self):
+        raise ("area() is not implemented")
