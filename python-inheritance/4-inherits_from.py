6c17ab367c73768d4ca4c1b7f87371887f598d7c
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-09 14:46:59 +0100

    corrected

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,7 +10,5 @@ def inherits_from(obj, a_class):
     """
     checks
     """
-    if isinstance(object, a_class):
-        return True
     if issubclass(type(obj), a_class) and not isinstance(obj, a_class):
         return True
