5a694e013aa41bc3a3ad60a19c9d8e320e5aaef3
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-09 19:19:03 +0100

    corrected attributes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -15,5 +15,5 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.integer_validator("height", width)
         self.integer_validator("height", height)
-        self.__width__ = width
-        self.__height__ = height
+        self.__width = width
+        self.__height = height
