c3746744d2bf1c19d72ce1233ad9b363c0a8ed93
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-10 14:11:50 +0100

    task 10 completed

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Square module
+'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    square rectangle
+    """
+
+    def __init__(self, size):
+        """
+        instanciation method
+        """
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        method to calculate the area
+        """
+        return self.__size**2
