077f39c5026f71c9c80c3e0a8cde14a99c4623a8
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-10 15:26:37 +0100

    last task pep8 11-square.py !

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+Square module
+'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    square rectangle
+    """
+
+    def __init__(self, size):
+        """
+        instanciation method
+        """
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        method to calculate the area
+        """
+        return self.__size**2
+
+    def __str__(self):
+        """
+        returns string representation
+        """
+        return f"[Square] {self.__size}/{self.__size}"
