40793167032663c07d9d01b44e8a6e432d671f36
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-12 14:05:30 +0100

    tadk 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,19 +4,18 @@ import sys
 import json
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__(
+    '6-load_from_json_file').load_from_json_file
 
-    file = "add_item.json"
-    args = sys.argv[1:]
+file = "add_item.json"
+args = sys.argv[1:]
 
-    try:
-        list = load_from_json_file(file)
+try:
+    list = load_from_json_file(file)
 
-    except FileNotFoundError:
-        my_list = []
-    for arg in args:
-        my_list.append(arg)
-    save_to_json_file(list, file)
+except FileNotFoundError:
+    my_list = []
+for arg in args:
+    my_list.append(arg)
+save_to_json_file(list, file)
