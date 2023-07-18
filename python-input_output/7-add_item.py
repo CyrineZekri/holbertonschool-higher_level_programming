abe08caa56da996cb675ece4b3e1c6206d54035d
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-18 22:19:29 +0100

    task 7 finallyyyy

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,20 +2,13 @@
 """module"""
 import sys
 import json
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__(
-    '6-load_from_json_file').load_from_json_file
-
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file = "add_item.json"
-args = sys.argv[1:]
-
+arguments = sys.argv[1:]
 try:
-    list = load_from_json_file(file)
-
+    data = load_from_json_file(file)
 except FileNotFoundError:
-    list = []
-for arg in args:
-    list.append(arg)
-save_to_json_file(list, file)
+    data = []
+data.extend(arguments)
+save_to_json_file(data, file)
