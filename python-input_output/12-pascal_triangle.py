967737041cdfed3c8cdf0f9b7b7c36053aaf3dbd
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-20 13:58:57 +0100

    Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) < n:
+        prev_row = triangle[-1]
+        new_row = [1] + [prev_row[i] + prev_row[i + 1]
+                         for i in range(len(prev_row) - 1)] + [1]
+        triangle.append(new_row)
+
+    return triangle
