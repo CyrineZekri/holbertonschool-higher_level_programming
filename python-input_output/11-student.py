fdb53f239609fbd214e5790bb2f4b2f7863158d0
CyrineZekri <cyrinezekri4@gmail.com>
2023-07-20 13:46:06 +0100

    the student representation

---
+++ python-input_output/11-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" student class module"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None and isinstance(attrs, list):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        self.__dict__.update(json)
