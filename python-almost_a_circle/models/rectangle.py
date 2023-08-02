9536da415c3e17f941243e598ddb71248a3f63ae
CyrineZekri <cyrinezekri4@gmail.com>
2023-08-02 06:26:06 +0100

    task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""module with Rectangle Class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle Class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """class constructer"""
+        super.__init__(id)
+        self.__width = width
+        self.__height = height
+        __x = x
+        __y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
