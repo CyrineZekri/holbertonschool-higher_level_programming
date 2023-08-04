5a71322ba5c5cbac62b58b2b6c2c0a711dad0801
CyrineZekri <cyrinezekri4@gmail.com>
2023-08-04 13:33:26 +0100

    to json file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Module with the Base model"""
+import json
 
 
 class Base:
@@ -13,3 +14,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """JSON representation"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return (json.dumps(list_dictionaries))
