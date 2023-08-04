49efda8aac83e7c9584d23a674bccafcc32b041c
CyrineZekri <cyrinezekri4@gmail.com>
2023-08-04 12:30:24 +0100

    requirement fixed

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -5,6 +5,8 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square class"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """method constructor"""
         super().__init__(size, size, x, y, id)
