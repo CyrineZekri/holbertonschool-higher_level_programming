47c366474d09168494554f240b7487543e3fd4cc
cyrine <cyrinezekri4@gmail.com>
2023-11-25 23:47:24 +0100

    script

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        document.getElementById('hello').textContent = data.hello;
+      })
+      .catch(function(error) {
+        console.error('Error:', error);
+      });
+  });
+  
